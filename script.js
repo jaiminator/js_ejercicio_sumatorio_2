4ad052aa867e29cbd182c6c8bfafd3868ba7f0af
jaime-lopez <jaimelopezmislata@gmail.com>
2025-02-05 11:59:13 +0100

    Codigo sin comentarios

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//A CADA BOTÓN LE AÑADIMOS UN EVENTO PARA LLAMAR A SU FUNCIÓN AL DARLE CLICK
 const buttonAdd = document.getElementById("buttonAdd")
 buttonAdd.addEventListener("click", addNumber)
 const buttonResult = document.getElementById("buttonResult")
@@ -8,13 +7,10 @@ const noOrderList = document.getElementById("noOrderList")
 
 let result = 0
 function addNumber() {
-    //CREAMOS UN ELEMENTO 'li' Y LE INSERTAMOS TEXTO
     const elementLi = document.createElement("li");
     let idNumber = document.getElementById("idNumber").value
     elementLi.innerText = "Numero " + idNumber;
-    //INSERTAMOS EL ELEMENTO DENTRO DE LA LISTA NO ORDENADA (ul)
     document.getElementById("noOrderList").appendChild(elementLi);
-    //AL RESULTADO LE VAMOS SUMANDO EL VALOR NUMÉRICO DEL INPUT
     result += Number(idNumber)
 }
 
